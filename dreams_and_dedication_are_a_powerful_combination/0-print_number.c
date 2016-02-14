5408d1b0fe13695457a843eec18f3373e6699ccb
Damian <damian.ali33@gmail.com>
2016-02-13 20:33:39 -0800

    Finished -98 Finished task: changing n to ncopy for the purpose of pushing the positive version of 98 through the while loop so it can be broken down, was the issue

--- dreams_and_dedication_are_a_powerful_combination/0-print_number.c
+++ dreams_and_dedication_are_a_powerful_combination/0-print_number.c
@@ -6,30 +6,31 @@ void print_number(int n){
  int power=10;
  int print;
 
+
+ if (n < 0){
+   n = n * -1;
+   print_char('-');
+ }
+
+ ncopy = n;
+
  while (ncopy > 9){
     ncopy=(ncopy / 10);
-    power=power*10;
+    power= power * 10;
    }
 
  power = power / 10;
  ncopy = n;
-
+ 
   while (ncopy > 0){
-    print=(ncopy / power);
-    ncopy=(ncopy%power);
-    power=(power / 10);
+    print= ncopy / power;
+    ncopy= ncopy % power;
+    power= power / 10;
     print_char(print + '0');
   }
 
   if (n == 0){
     print_char('0');
     }
-
-  if (ncopy < 0){
-    ncopy = n - '0';
-    ncopy = ncopy * -1;
-    print_char('-');
-    print_char(ncopy + '0');
-  }
 
 }
