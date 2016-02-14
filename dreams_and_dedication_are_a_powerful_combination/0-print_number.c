4927be7291b8d57b849be3b63a1ed42a7297dd4f
Damian <damian.ali33@gmail.com>
2016-02-13 19:11:47 -0800

    trying a different angle

--- dreams_and_dedication_are_a_powerful_combination/0-print_number.c
+++ dreams_and_dedication_are_a_powerful_combination/0-print_number.c
@@ -1,38 +1,35 @@
 #include "my_functions.h"
 
 void print_number(int n){
-
+  
  int ncopy=n;
- int count=1;
- int split=1;
- 
-  while (ncopy > 9){
-    ncopy=ncopy/10;
-    split=split*10;
-    count++;
+ int power=10;
+ int print;
+
+ if (n < 0){
+   ncopy = n * -1;
+   print_char('-');
+   print_char(n + '0');
+ }
+
+ while (ncopy > 9){
+    ncopy=(ncopy / 10);
+    power=power*10;
    }
 
-  for (ncopy=n/split;ncopy>9;split=split/10){
-    ncopy=ncopy/split;
-    print_char('n');
-   }
+ power = power / 10;
+ ncopy = n;
 
-  while (ncopy == 0){
+  while (ncopy > 0){
+    print=(ncopy / power);
+    ncopy=(ncopy%power);
+    power=(power / 10);
+    print_char(print + '1');
+  }
+
+  if (n == 0){
     print_char('0');
-   }
+    }
+
 
 }
-
-
-
-
-
-
-
-
-
-
-  
-
-    
-  
