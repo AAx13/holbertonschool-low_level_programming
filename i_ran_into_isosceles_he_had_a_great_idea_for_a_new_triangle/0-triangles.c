920c5d9580c0a487925f3f208d2ca6666da5744b
Konflict <Konflict@Konflicts-MacBook-Pro.local>
2016-02-27 19:46:06 -0800

    Added comments to the program

--- i_ran_into_isosceles_he_had_a_great_idea_for_a_new_triangle/0-triangles.c
+++ i_ran_into_isosceles_he_had_a_great_idea_for_a_new_triangle/0-triangles.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
-void triangles_in_the_term(int h, int n){
+void triangles_in_the_term(int h, int n){                   /* This function prints triangles of stars of a given height (h) and a given amount of triangles (n) */
   
   int space;
   int height;
   int star;
   int ncopy;
   
-  for(ncopy = 1; ncopy <= n ; ncopy++){
-    for(height = 1 ; height <= h ; height++){
-      for(space = h - height ; space >= 1 ; space--){
+  for(ncopy = 1; ncopy <= n ; ncopy++){                      /* This loop iterates to allow for additional amounts of triangles */
+    for(height = 1 ; height <= h ; height++){                /* This loop iterates through to account for height each iteration */
+      for(space = h - height ; space >= 1 ; space--){        /* This loop prints spaces before each initial star within each line */
 	printf(" ");
       }
-      for(star = 1 ; star <= height * 2 -1 ; star++){
+      for(star = 1 ; star <= height * 2 -1 ; star++){        /* This loop prints the amount of stars needed to complete the triangle */
 	printf("*");
     }
       printf("\n");
