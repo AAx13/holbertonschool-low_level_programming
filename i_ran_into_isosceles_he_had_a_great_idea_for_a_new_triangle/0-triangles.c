5488efcd1adc728b7ce2308d9c563382082ec0a7
Konflict <Konflict@Konflicts-MacBook-Pro.local>
2016-02-27 19:38:31 -0800

    Fixed program to include standard library

--- i_ran_into_isosceles_he_had_a_great_idea_for_a_new_triangle/0-triangles.c
+++ i_ran_into_isosceles_he_had_a_great_idea_for_a_new_triangle/0-triangles.c
@@ -1,4 +1,4 @@
-int print_char(char c);
+#include <stdio.h>
 
 void triangles_in_the_term(int h, int n){
   
@@ -10,12 +10,12 @@ void triangles_in_the_term(int h, int n){
   for(ncopy = 1; ncopy <= n ; ncopy++){
     for(height = 1 ; height <= h ; height++){
       for(space = h - height ; space >= 1 ; space--){
-	print_char(' ');
+	printf(" ");
       }
       for(star = 1 ; star <= height * 2 -1 ; star++){
-	print_char('*');
+	printf("*");
     }
-      print_char('\n');
+      printf("\n");
     }
   }
   
