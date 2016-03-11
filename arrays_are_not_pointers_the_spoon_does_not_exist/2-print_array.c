b3d60580f719f32be01c15dcffa58b4a0892a63e
Konflict <Konflict@Konflicts-MacBook-Pro.local>
2016-03-10 17:43:41 -0800

    Finished Tasks and updated code

--- arrays_are_not_pointers_the_spoon_does_not_exist/2-print_array.c
+++ arrays_are_not_pointers_the_spoon_does_not_exist/2-print_array.c
@@ -6,47 +6,54 @@ void print_number(int n){
   int power=10;
   int print;
 
-
-  if (n < 0){
-    n = n * -1;
-    print_char('-'); }
-
-  ncopy = n;
-
-  while (ncopy > 9){
+  while (ncopy > 9 || ncopy < -9){
     ncopy=(ncopy / 10);
-    power= power * 10; }
+    power= power * 10;
+  }
 
-  power = power / 10;
   ncopy = n;
-
-  while (ncopy > 0){
+ 
+  if(ncopy < 0){
+    print_char('-');
+  }
+ 
+  power = power / 10;
+  
+  if(n > 0){
+    ncopy= n * -1;
+  }
+  
+  while (ncopy < 0){
     print= ncopy / power;
     ncopy= ncopy % power;
     power= power / 10;
-    print_char(print + '0'); }
+    print = print * -1;
+    print_char(print + '0');
+  }
 
   if (n == 0){
-    print_char('0'); }}
-
-/* This function takes a given number and prints it */
-/* In order to do this consiering I did not use any of the standard libraries to acheive this, I had to figure out how to tell the computer two things. */
-/* 1. How to tell the computer what numbers needed to be printed by identifying each digit */
-/* 2. Identifying how many digits there are. */
-
-void print_array(int *a, int n){ /* This function will print each element in an array */
-
-  int i;                         
-
-  for(i = 1; i < n; i++){     /* after initializing "i" I will count that starting at one and iterate up through the amount of elements in the array */
-    print_number(*a);         /* once checked for the condition less than the max number of elements in the array it will print */
-    print_char(',');          /* the dereferenced value of the array which would be the next element in the array and immediately following this print comma and space */
-    print_char(' ');
-    a++;
-  } if( i == n){              /* once i reachest the last element in the array it will print only the element as well as a return as specified */
-    print_number(*a);         
-    print_char('\n');
+    print_char('0');
   }
 }
+
+  /* This function takes a given number and prints it */
+  /* In order to do this consiering I did not use any of the standard libraries to acheive this, I had to figure out how to tell the computer two things. */
+  /* 1. How to tell the computer what numbers needed to be printed by identifying each digit */
+  /* 2. Identifying how many digits there are. */
+
+  void print_array(int *a, int n){ /* This function will print each element in an array */
+
+    int i;                         
+
+    for(i = 1; i < n; i++){     /* after initializing "i" I will count that starting at one and iterate up through the amount of elements in the array */
+      print_number(*a);         /* once checked for the condition less than the max number of elements in the array it will print */
+      print_char(',');          /* the dereferenced value of the array which would be the next element in the array and immediately following this print comma and space */
+      print_char(' ');
+      a++;
+    } if( i == n){              /* once i reachest the last element in the array it will print only the element as well as a return as specified */
+      print_number(*a);         
+      print_char('\n');
+    }
+  }
 
 
