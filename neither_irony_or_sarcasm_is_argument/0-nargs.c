8c1f13caf1424098d8824c487fd2ca25cf2e5e13
DamianAli <damian.ali33@gmail.com>
2016-04-04 13:49:57 -0700

    Tasks commented

--- neither_irony_or_sarcasm_is_argument/0-nargs.c
+++ neither_irony_or_sarcasm_is_argument/0-nargs.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-int main(int argc, char **argv){
+int main(int argc, char **argv){  /* this program will return the number of arguements input */
 
   argc -= 1;
   argv[0] -= 0;
--- neither_irony_or_sarcasm_is_argument/1-args.c
+++ neither_irony_or_sarcasm_is_argument/1-args.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-int main(int argc, char **argv){
+int main(int argc, char **argv){ /* this program will print each and every argument */
 
   int i;
 
