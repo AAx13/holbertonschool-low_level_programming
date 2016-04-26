db1d56be97cdf72eeb7a55c67d802dc02a9feb6d
DamianAli <damian.ali33@gmail.com>
2016-04-26 14:54:23 -0700

    Finished task 5 (commented)

---
+++ cisdoublefun_basics/5-odd.c
@@ -0,0 +1,12 @@
+
+char odd(int n)
+{
+  if(n % 2 == 0)       /* checks to ensure the number is even and return 'E' */
+  {
+    return 'E';
+  }
+  else
+  {
+    return 'O';      /* otherwise return '0' */
+  }
+}
