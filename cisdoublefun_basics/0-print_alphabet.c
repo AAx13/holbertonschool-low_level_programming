741ba9c97cc1240be200721d5bbc16527a954822
DamianAli <damian.ali33@gmail.com>
2016-04-26 13:35:13 -0700

    Commented task 0

--- cisdoublefun_basics/0-print_alphabet.c
+++ cisdoublefun_basics/0-print_alphabet.c
@@ -1,13 +1,13 @@
-int print_char(char c);
+int print_char(char c);         /* added print_char prototype for print_char function use */
 void print_alphabet(void)
 {
-  int i;
+  int i;              /* declaring variable i which we'll use as to iterate through the alphabet */
 
-  i = 97;
+  i = 97;           /* making variable i 97 would make its ascii character value 'a' */
 
-  while(i < 123)
+  while(i < 123)    /* as long as i is less than the ascii value of 'z' + 1 */
   {
-      print_char(i);
+      print_char(i);   /* print that character and iterate up one */
       i++;
   }
 
