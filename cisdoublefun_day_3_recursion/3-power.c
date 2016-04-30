a07a8d9c9acec3787dd67f158c61f98b80bd7bfc
DamianAli <damian.ali33@gmail.com>
2016-04-30 12:20:55 -0700

    Finished task 3 (correction)

--- cisdoublefun_day_3_recursion/3-power.c
+++ cisdoublefun_day_3_recursion/3-power.c
@@ -10,7 +10,7 @@ int power(int x, int y)         /* this function will recursively calculate the 
     return 1;
   } else if(x > INT_MAX / power(x, (y - 1)))       /* this if statement will return -1 if the result is bigger than INT_MAX */
   {
-    return -1;
+     return -1;
   }
   return x * power(x, (y - 1));            /* this return will recursively allow for this program to calculate the power of a number */
 }
