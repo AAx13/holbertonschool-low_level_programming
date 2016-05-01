084d8371bfd6e7d590ec0ad7d74f33ba7dfbc641
DamianAli <damian.ali33@gmail.com>
2016-04-30 23:18:59 -0700

    pushing what I have on string_compare

---
+++ cisdoublefun_day_4_more_pointers/4-strings_compare.c
@@ -0,0 +1,14 @@
+
+int strings_compare(char *s1, const char *s2)
+{
+  int i, j;
+
+  i = 0, j = 0;
+  while(s1[i] != '\0' && s2[i] != '\0')
+  {
+    if(s1[i] > s2[i])
+
+    i++;
+  }
+  return 0;
+}
