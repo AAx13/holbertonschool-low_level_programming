6fbf039c3c1dfe6ad871006ae1440a65d1c795f4
DamianAli <damian.ali33@gmail.com>
2016-05-03 19:27:28 -0700

    Finished task 1 (commented)

--- cisdoublefun_day_7_makefile_pointers_to_functions/1-array_iterator.c
+++ cisdoublefun_day_7_makefile_pointers_to_functions/1-array_iterator.c
@@ -1,5 +1,12 @@
 
-void array_iterator(int *, int, void (*)(int))
-{
+void array_iterator(int *array, int n, void (*ptr_action)(int)) /* this is a function that executes another function */
+{                                                               /* given as a parameter on each element of an array. */
+  int i;
 
+  i = 0;
+  while(i < n)
+  {
+    ptr_action(array[i]);
+    i++;
+  }
 }
