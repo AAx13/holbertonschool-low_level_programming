74024835975abbacc5dc84734c9b3bcb44e4381d
DamianAli <damian.ali33@gmail.com>
2016-05-06 11:59:06 -0700

    Finished task 0 (commented)

---
+++ linked_lists/0-add_node.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include "list.h"
+#include <stdio.h>
+int str_length(char *str);
+
+int add_node(List **list, char *content)  /* this function will add a new node to a linked list */
+{
+  List *node1;
+  int i;
+  int len;
+  char *newstr;
+
+  node1 = malloc(sizeof(List));         /* allocate memory to match the size of the previous struct */
+
+  len = str_length(content);            /* return the length of the string soon to be copied */
+  newstr = malloc(sizeof(char) * len);   /* allocating enough memory to store a copy of the string */
+  for(i = 0; content[i] != '\0'; i++)   /* this loop will copy a string to newly allocated area */
+  {
+    newstr[i] = content[i];
+  }
+  newstr[i] = '\0';
+
+  node1->str = newstr;
+  node1->next = *list;    /* change next within the struct to point to the address of the next element which was the first previously */
+  *list = node1;         /* change the address of the first element in the list to the new node */
+
+  if(node1 == NULL)
+  {
+    return 1;
+  }
+  return 0;
+}
+
+int str_length(char *str)    /* this function returns the length of a string */
+{
+  int i;
+  for(i = 0; str[i] != '\0'; i++);
+  return i;
+}
