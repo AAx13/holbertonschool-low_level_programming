64434a2606f8cee813218c9224beeb0a70791dc8
DamianAli <damian.ali33@gmail.com>
2016-05-07 11:14:59 -0700

    Finished task 5 (commented)

--- linked_lists/4-free_list.c
+++ linked_lists/4-free_list.c
@@ -1,5 +1,13 @@
+#include "list.h"
+#include <stdlib.h>
 
-void free_list(List *list)
+void free_list(List *list) /* this function will free the memory off all elements in the list */
 {
+  List *newNode;
 
+  for(newNode = list; newNode != NULL; newNode = newNode->next)
+  {
+    free(newNode->str);
+    free(newNode);
+  }
 }
