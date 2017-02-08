c8dbf88e861532be81c579d43ce61eae2a2760d6
DamianAli <damian.ali33@gmail.com>
2017-02-07 20:38:01 -0800

    Task 9- Test cases.

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,11 +16,17 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int n)
 	unsigned int i;
 
 	cur_node = *head;
+
+	new_node = malloc(sizeof(listint_t));
+	if (!new_node)
+	{
+		return (NULL);
+	}
+
 	for (i = 0; cur_node != NULL; i++)
 	{
 		if (index == 0)
 		{
-			new_node = malloc(sizeof(listint_t));
 			new_node->n = n;
 			new_node->next = *head;
 			*head = new_node;
@@ -28,7 +34,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int n)
 		}
 		else if (i == index - 1)
 		{
-			new_node = malloc(sizeof(listint_t));
 			new_node->n = n;
 			new_node->next = cur_node->next;
 			cur_node->next = new_node;
