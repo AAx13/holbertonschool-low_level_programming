28b8eb15e77205d362bd677c808d6a26358c7e2e
DamianAli <damian.ali33@gmail.com>
2017-03-05 21:29:41 -0800

    run against hbtn check.

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -17,9 +17,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 	{
 		return (NULL);
 	}
+	new_ht->size = 0;
 
-	new_ht->size = size;
-	new_ht->array = malloc(sizeof(hash_node_t) * size);
+	new_ht->array = malloc(sizeof(hash_node_t *) * size);
 	if(!new_ht->array)
 	{
 		return (NULL);
