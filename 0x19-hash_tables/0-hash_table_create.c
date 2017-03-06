93b6846988f9135333a09a0f22cd2cdac36392ec
DamianAli <damian.ali33@gmail.com>
2017-03-05 19:56:24 -0800

    Task 0- run against hbtn check.

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -1,11 +1,16 @@
+#include <stdlib.h>
 #include "hash_tables.h"
 
 /**
  * hash_table_create - creates a hash table.
  * @size: yep
  *
- * Return: 
+ * Return:
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	hash_table_t *new_table;
+
+	new_table = malloc(sizeof(hash_table_t) * size);
+	return (new_table);
 }
