417d9dea085afd15786413ed50164af4ccca4178
DamianAli <damian.ali33@gmail.com>
2017-03-05 22:53:09 -0800

    Task 1- complete, setup for Task 2, runing Task 1 against hbtn checks.

---
+++ 0x19-hash_tables/2-key_index.c
@@ -0,0 +1,12 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - Function to return the hash of a key.
+ * @key: Key to look up.
+ * @size: Size of the array of hash of the hash table.
+ *
+ * Return: index at which the key/value pair in the array of the hash table.
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+}
--- 0x19-hash_tables/hash_tables.h
+++ 0x19-hash_tables/hash_tables.h
@@ -33,4 +33,10 @@ typedef struct hash_table_s
 /* hash_table_create - creates a hash table. */
 hash_table_t *hash_table_create(unsigned long int size);
 
+/* hash_dbj2 - Hash function implementing the djb2 algorithm. */
+unsigned long int hash_djb2(const unsigned char *str);
+
+/* key_index - Function to return the hash of a key.*/
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+
 #endif /* HASH_TABLES */
