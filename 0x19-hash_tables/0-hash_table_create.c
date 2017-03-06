87c4c4a2073035fddd2411ae91040c76f7939c74
DamianAli <damian.ali33@gmail.com>
2017-03-05 22:09:16 -0800

    run against hbtn check.

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -2,10 +2,10 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - creates a hash table.
- * @size: yep
+ * hash_table_create - Creates a hash table.
+ * @size: Size of the array.
  *
- * Return:
+ * Return: Pointer to new hash table.
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
@@ -13,14 +13,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int i;
 
 	new_ht = malloc(sizeof(hash_table_t));
-	if(!new_ht || size < 1)
+	if (!new_ht || size < 1)
 	{
 		return (NULL);
 	}
 	new_ht->size = 0;
 
 	new_ht->array = malloc(sizeof(hash_node_t *) * size);
-	if(!new_ht->array)
+	if (!new_ht->array)
 	{
 		return (NULL);
 	}
