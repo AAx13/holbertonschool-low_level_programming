42e8749e8fed246b68c6a40b30a6687f7191cdf9
DamianAli <damian.ali33@gmail.com>
2017-03-05 20:04:43 -0800

    Task 0- run against hbtn check.

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -12,10 +12,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new_table;
 
 	new_table = malloc(sizeof(hash_table_t) * size);
-	if (new_table)
+	if (!new_table)
 	{
-		return (new_table);
+		return (NULL);
 	}
+	new_table->size = 0;
+	new_table->array = NULL;
 
-	return (NULL);
+	return (new_table);
 }
