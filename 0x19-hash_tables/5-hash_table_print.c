74d75807b0fb614af342da6fa47ffad2048df1e5
DamianAli <damian.ali33@gmail.com>
2017-03-06 23:59:14 -0800

    Task 5- run against hbtn check.

--- 0x19-hash_tables/5-hash_table_print.c
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -1,11 +1,49 @@
-#include "hash_table.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "hash_tables.h"
 
 /**
  * hash_table_print - Prints a hash table.
- * @ht: a hash table.
+ * @ht: A hash table.
  *
  * Return: Void.
  */
 void hash_table_print(const hash_table_t *ht)
 {
+	unsigned long int i, first, last, len, sentinel;
+	hash_node_t *node;
+
+	len = 0;
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i])
+		{
+			len++;
+		}
+	}
+
+	first = 1;
+	last = len;
+	sentinel = 0;
+	for (i = 0; i < ht->size; i++)
+	{
+		for (node = ht->array[i]; node; node = node->next)
+		{
+			sentinel++;
+			if (sentinel == first)
+			{
+				printf("{'%s': '%s', ", node->key, node->value);
+			}
+			else if(sentinel == last)
+			{
+				printf("'%s': '%s'}\n", node->key, node->value);
+			}
+			else
+			{
+				printf(" '%s': '%s', ", node->key, node->value);
+			}
+		}
+	}
+
+
 }
