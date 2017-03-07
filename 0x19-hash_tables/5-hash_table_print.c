852f05a77c601ae8f008abe657d7cb550e005de2
DamianAli <damian.ali33@gmail.com>
2017-03-07 14:02:11 -0800

    Task 5- run against hbtn checks.

--- 0x19-hash_tables/5-hash_table_print.c
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -33,11 +33,11 @@ void hash_table_print(const hash_table_t *ht)
 		}
 		else if (i == x - 1)
 		{
-			printf(" '%s': '%s'}", node->key, node->value);
+			printf("'%s': '%s'}", node->key, node->value);
 		}
 		else
 		{
-			printf(" '%s': '%s',", node->key, node->value);
+			printf("'%s': '%s', ", node->key, node->value);
 		}
 	}
 	printf("\n");
