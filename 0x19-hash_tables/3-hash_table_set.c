f751cdc392acb47565347502f51072be259daac9
DamianAli <damian.ali33@gmail.com>
2017-03-07 20:53:17 -0800

    test.

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -18,7 +18,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	sentinel = 0;
 	new_node = malloc(sizeof(hash_node_t));
-	if (!new_node || !key || !ht)
+	if (!new_node || !key || !ht || !value)
 	{
 		return (0);
 	}
