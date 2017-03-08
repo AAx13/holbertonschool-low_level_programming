7dfa77c41669656f0c2a5e98970ab26f5010d391
DamianAli <damian.ali33@gmail.com>
2017-03-07 23:58:33 -0800

    Task 5- run against hbtn checks.

--- 0x19-hash_tables/5-hash_table_print.c
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -1,72 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "hash_tables.h"
-
-/**
- * print_list - Prints list containing more than one node.
- * @ht: A hash table.
- * @buffer: Buffer contains keys from all elements in the array.
- * @x: Length of the buffer.
- *
- * Return: Void.
- */
-void print_list(const hash_table_t *ht, unsigned long int *buffer,
-		unsigned long int x)
-{
-	unsigned long int i;
-	hash_node_t *node;
-
-	for (i = 0; i < x; i++)
-	{
-		node = ht->array[buffer[i]];
-		if (node->next)
-		{
-			while (node)
-			{
-				printf(", '%s': '%s'", node->key, node->value);
-				node = node->next;
-			}
-		}
-	}
-}
-
-/**
- * print_array - Prints array of nodes.
- * @ht: A hash table.
- * @buffer: Buffer contains keys from all elements in the array.
- * @x: Length of the buffer.
- *
- * Return: Void.
- */
-void print_array(const hash_table_t *ht, unsigned long int *buffer,
-		 unsigned long int x)
-{
-	unsigned long int i;
-	hash_node_t *node;
-
-	printf("{");
-	for (i = 0; i < x; i++)
-	{
-		node = ht->array[buffer[i]];
-		if (!node->next)
-		{
-			if (i == 0)
-			{
-				printf("'%s': '%s'", node->key, node->value);
-			}
-			else
-			{
-				printf(", '%s': '%s'", node->key, node->value);
-			}
-		}
-		else
-		{
-			/* if there is more than one node */
-			print_list(ht, buffer, x);
-		}
-	}
-	printf("}\n");
-}
 
 /**
  * hash_table_print - Prints a hash table.
@@ -77,8 +10,7 @@ void print_array(const hash_table_t *ht, unsigned long int *buffer,
 void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i, x;
-	unsigned long int *buffer;
-	hash_node_t *node;
+ 	hash_node_t *node;
 
 	x = 0;
 	if (!ht || !ht->array)
@@ -86,19 +18,21 @@ void hash_table_print(const hash_table_t *ht)
 		return;
 	}
 
-	buffer = malloc(sizeof(int) * ht->size);
+	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
 		for (node = ht->array[i]; node; node = node->next)
 		{
-			buffer[x] = key_index(
-				(const unsigned char *)node->key, ht->size);
-			if (buffer[x] == buffer[x - 1])
+			if (x == 0)
 			{
-				break;
+				printf("'%s': '%s'", node->key, node->value);
+			}
+			else
+			{
+				printf(", '%s': '%s'", node->key, node->value);
 			}
 			x++;
 		}
 	}
-	print_array(ht, buffer, x);
+	printf("}\n");
 }
