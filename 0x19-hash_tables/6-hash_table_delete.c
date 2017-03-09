d6375fcefaa252137ed19457839dcfd381934eb9
DamianAli <damian.ali33@gmail.com>
2017-03-09 00:36:53 -0800

    Task 6- run against hbtn checks.

--- 0x19-hash_tables/6-hash_table_delete.c
+++ 0x19-hash_tables/6-hash_table_delete.c
@@ -1,3 +1,7 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
 /**
  * hash_table_delete - Deletes a  hash table.
  * @ht: A hash table.
@@ -6,4 +10,20 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
+	unsigned long int i;
+	hash_node_t *node, *tmp_node;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		for (node = ht->array[i]; node;)
+		{
+			tmp_node = node;
+			node = node->next;
+			free(tmp_node->key);
+			free(tmp_node->value);
+			free(tmp_node);
+		}
+	}
+	free(ht->array);
+	free(ht);
 }
