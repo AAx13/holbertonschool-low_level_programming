d7e04b5ee6dc69e20915f08e32ca8689275f2d39
DamianAli <damian.ali33@gmail.com>
2017-03-10 22:17:44 -0800

    Task 7- Complete.

---
+++ 0x07-recursion/7-is_palindrome.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+int _strlen_recursion(char *s)
+{
+	if (*s)
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+
+	return (0);
+}
+
+int palindrome_iter(char *s, char *z, int i)
+{
+	if (i / 2 == i)
+	{
+		return (1);
+	}
+
+	if (*s != z[i])
+	{
+		return (0);
+	}
+
+	return (palindrome_iter(s + 1, z, i - 1));
+}
+
+int is_palindrome(char *s)
+{
+	int i;
+	char *z;
+
+	z = s;
+	i = _strlen_recursion(s) - 1;
+
+	return (palindrome_iter(s, z, i));
+}
--- 0x07-recursion/holberton.h
+++ 0x07-recursion/holberton.h
@@ -25,4 +25,7 @@ int _sqrt_recursion(int n);
 /* */
 int is_prime_number(int n);
 
+/* */
+int is_palindrome(char *s);
+
 #endif /* HOLBERTON */
