5e5013f4f08994b8871403cf9e93f5d7c1ee7110
DamianAli <damian.ali33@gmail.com>
2017-03-10 23:54:35 -0800

    Dan

--- 0x07-recursion/7-is_palindrome.c
+++ 0x07-recursion/7-is_palindrome.c
@@ -10,28 +10,26 @@ int _strlen_recursion(char *s)
 	return (0);
 }
 
-int palindrome_iter(char *s, char *z,  int i)
+int palindrome_iter(char *s, int i)
 {
-	if (i == 0)
+	if (i < 1)
 	{
 		return (1);
 	}
 
-	if (*s != *(z + i))
+	if (*s != *(s + i))
 	{
 		return (0);
 	}
 
-	return (palindrome_iter(s + 1, z, i - 1));
+	return (palindrome_iter(s + 1, i - 2));
 }
 
 int is_palindrome(char *s)
 {
 	int i;
-	char *z;
 
-	z = s;
 	i = _strlen_recursion(s) - 1;
 
-	return (palindrome_iter(s, z, i));
+	return (palindrome_iter(s, i));
 }
