4b3c8c4ca8832fdd7dcb20ace5deaec353870646
DamianAli <damian.ali33@gmail.com>
2017-03-10 19:06:27 -0800

    Task 1, 2, 3- Complete.

--- 0x07-recursion/0-puts_recursion.c
+++ 0x07-recursion/0-puts_recursion.c
@@ -2,14 +2,12 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s)
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
-	else
+	if (!*s)
 	{
 		_putchar('\n');
 		return;
 	}
+
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
