a04f2607bdc455d15d965e13df9d7b5949f03c9c
DamianAli <damian.ali33@gmail.com>
2017-03-14 18:45:23 -0700

    Task 6->7- added doc to functions.

--- 0x07-recursion/6-is_prime_number.c
+++ 0x07-recursion/6-is_prime_number.c
@@ -1,5 +1,12 @@
 #include "holberton.h"
 
+/**
+ * prime_iterate - finds out if a number is a prime number.
+ * @n: A number to evaluate as prime or not.
+ * @i: A number used to find the prime number.
+ *
+ * Return: 1 if the input is a prime number, otherwise return 0.
+ */
 int prime_iterate(int n, int i)
 {
 	if (n <= 1)
@@ -20,6 +27,13 @@ int prime_iterate(int n, int i)
 	return (prime_iterate(n, i + 1));
 }
 
+/**
+ * is_prime_number - returns 1 if the input integer is a prime number,
+ * otherwise return 0.
+ * @n: A number to evaluate as prime or not.
+ *
+ * Return: 1 if the input is a prime number, otherwise return 0.
+ */
 int is_prime_number(int n)
 {
 	int i;
--- 0x07-recursion/7-is_palindrome.c
+++ 0x07-recursion/7-is_palindrome.c
@@ -1,5 +1,11 @@
 #include "holberton.h"
 
+/**
+ * _strlen_recursion - returns the length of a string.
+ * @s: A string.
+ *
+ * Return: The length of the string (int).
+ */
 int _strlen_recursion(char *s)
 {
 	if (*s)
@@ -10,6 +16,13 @@ int _strlen_recursion(char *s)
 	return (0);
 }
 
+/**
+ * palindrome_iter - returns 1 if a string is a palindrome and 0 if not.
+ * @s: A string.
+ * @i: Length of the string used to compare characters from the string.
+ *
+ * Return: 1 if a string is a palindrome and 0 if not.
+ */
 int palindrome_iter(char *s, int i)
 {
 	if (i < 1)
@@ -25,6 +38,12 @@ int palindrome_iter(char *s, int i)
 	return (palindrome_iter(s + 1, i - 2));
 }
 
+/**
+ * palindrome_iter - returns 1 if a string is a palindrome and 0 if not.
+ * @s: A string.
+ *
+ * Return: 1 if a string is a palindrome and 0 if not.
+ */
 int is_palindrome(char *s)
 {
 	int i;
