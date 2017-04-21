dd3f159ffa49b8e78056c09563049234fc1d4071
DamianAli <damian.ali33@gmail.com>
2017-04-20 23:12:02 -0700

    Task 7- no docks, possible refactoring needed based on length of functions.

--- 0x19-hash_tables/100-sorted_hash_table.c
+++ 0x19-hash_tables/100-sorted_hash_table.c
@@ -3,9 +3,81 @@
 #include <string.h>
 #include "hash_tables.h"
 
-/*void shash_table_delete(shash_table_t *ht);
-  void shash_table_print_rev(const shash_table_t *ht);*/
-/*char *shash_table_get(const shash_table_t *ht, const char *key);*/
+void shash_table_delete(shash_table_t *ht)
+{
+	unsigned long int i;
+	shash_node_t *node, *tmp_node;
+
+	if (!ht)
+	{
+		return;
+	}
+
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node)
+		{
+			tmp_node = node;
+			node = node->next;
+			free(tmp_node->key);
+			free(tmp_node->value);
+			free(tmp_node);
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
+
+void shash_table_print_rev(const shash_table_t *ht)
+{
+	int x;
+	shash_node_t *node;
+
+	x = 0;
+	if (!ht || !ht->stail)
+	{
+		return;
+	}
+
+	printf("{");
+	for (node = ht->stail; node; node = node->sprev)
+	{
+		if (x == 0)
+		{
+			printf("'%s': '%s'", node->key, node->value);
+		}
+		else
+		{
+			printf(", '%s': '%s'", node->key, node->value);
+		}
+		x++;
+	}
+	printf("}\n");
+}
+
+char *shash_table_get(const shash_table_t *ht, const char *key)
+{
+	shash_node_t *node;
+	unsigned long int index;
+
+	if (!ht || !key)
+	{
+		return (NULL);
+	}
+
+	index = key_index((const unsigned char *)key, ht->size);
+	for (node = ht->array[index]; node; node = node->next)
+	{
+		if (strcmp(key, node->key) == 0)
+		{
+			return (node->value);
+		}
+	}
+
+	return (NULL);
+}
+
 void shash_table_print(const shash_table_t *ht)
 {
 	int x;
@@ -36,7 +108,7 @@ void shash_table_print(const shash_table_t *ht)
 int shash_table_set(shash_table_t *ht, const char *key, const char *value)
 {
 	unsigned int sentinel;
-	shash_node_t *new_node, *tmp_node;
+	shash_node_t *new_node, *tmp_node, *pre_node;
 	unsigned long int index;
 
 	sentinel = 0;
@@ -87,18 +159,33 @@ int shash_table_set(shash_table_t *ht, const char *key, const char *value)
 	else /* add and sort new node */
 	{
 		tmp_node = ht->shead;
-		while (new_node->key > tmp_node->key) /* */
+		while (tmp_node->snext && strcmp(new_node->key, tmp_node->key) > 0)
 		{
-			tmp_node = tmp_node->snext;
+		        tmp_node = tmp_node->snext;
 		}
-		new_node = tmp_node->snext;
-		if (!new_node->snext)
+
+		if (tmp_node == ht->shead)
 		{
+			ht->shead = new_node;
+			new_node->snext = tmp_node;
+			new_node->sprev = NULL;
+			tmp_node->sprev = new_node;
+		}
+		else if (tmp_node->snext == NULL)
+		{
+			tmp_node->snext = new_node;
+			new_node->sprev = tmp_node;
 			new_node->snext = NULL;
+			ht->stail = new_node;
 		}
-		new_node->snext = tmp_node->snext->snext;
-		new_node->sprev = tmp_node;
-		ht->stail = new_node;
+		else
+		{
+			pre_node = tmp_node->sprev;
+			pre_node->snext = new_node;
+			new_node->snext = tmp_node;
+			new_node->sprev = pre_node;
+			tmp_node->sprev = new_node;
+		}
 	}
 
 	return (1);
